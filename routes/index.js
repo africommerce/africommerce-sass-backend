55cbb368290b10e672725e269e1461deaeca272e
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-22 22:57:28 +0100

    noop: added category routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const productRoute = require("../src/products/productRoute");
 const userRoute = require("../src/users/users")
+const categoryRoute = require("../src/categories/categoryRoute")
 
 const passport = require('passport')
 
@@ -15,6 +16,7 @@ rootRouter.get('/', function (req, res, next) {
 
 rootRouter.use('/users', userRoute);
 rootRouter.use('/products', productRoute)
+rootRouter.use('/category', categoryRoute)
 
 
 
