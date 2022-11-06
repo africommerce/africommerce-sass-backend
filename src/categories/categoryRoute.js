4c15c87b97b3feb32cfb4f06daa07c7ed38c4196
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-11-06 20:32:47 +0100

    noob: mounted five-random-categories route

--- src/categories/categoryRoute.js
+++ src/categories/categoryRoute.js
@@ -4,6 +4,10 @@ const categoryController = require('./categoryController')
 const categoryRoute = express.Router()
 
 categoryRoute
+    .route('/five-random-categories')
+    .get(categoryController.fiveRandomCategories)
+
+categoryRoute
     .route('/')
     .post(categoryController.createCategory)
     .get(categoryController.getAllCategories)
