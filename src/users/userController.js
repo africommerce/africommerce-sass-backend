63e9e69ecce83bc03bc99020d55988a691a30422
SAMUEL-UME <104633362+SAMUEL-UME@users.noreply.github.com>
2022-11-11 12:23:29 +0100

    Solved password should not be displayed

--- src/users/userController.js
+++ src/users/userController.js
@@ -1,138 +1,144 @@
-const { userModel } = require("../../model/users")
-var passport = require("passport")
-const { body, validationResult } = require("express-validator")
-const { hashPassword, validateUser } = require("../../config/helper")
+const { userModel } = require("../../model/users");
+var passport = require("passport");
+const { body, validationResult } = require("express-validator");
+const { hashPassword, validateUser } = require("../../config/helper");
 
-const authenticate = require('../../middleware/authenticate')
+const authenticate = require("../../middleware/authenticate");
 
 async function createUser(req, res) {
-    const { firstname, lastname, username, email, password, phonenumber } = req.body;
-    let userExist = await userModel.findOne({ username: username })
-    if (!userExist) {
-        userExist = await userModel.findOne({ email: email })
-    }
-    if (userExist) {
-        return res.status(409).send("This user already exist!")
-    }
-    const newUser = {
-        firstname,
-        lastname,
-        username,
-        email,
-        password,
-        phonenumber
-    }
-    const hashedPassword = await hashPassword(newUser.password)
-    newUser.password = hashedPassword
-    const user = await userModel.create(newUser)
-    res.json({
-        msg: "Registration successful!",
-        data: user
-    })
-};
+  const { firstname, lastname, username, email, password, phonenumber } =
+    req.body;
+  let userExist = await userModel.findOne({ username: username });
+  if (!userExist) {
+    userExist = await userModel.findOne({ email: email });
+  }
+  if (userExist) {
+    return res.status(409).send("This user already exist!");
+  }
+  const newUser = {
+    firstname,
+    lastname,
+    username,
+    email,
+    password,
+    phonenumber,
+  };
+  const hashedPassword = await hashPassword(newUser.password);
+  newUser.password = hashedPassword;
+  const user = await userModel.create(newUser);
+
+  if (user) {
+    const { password, ...others } = user._doc;
+    return res.json({
+      msg: "Registration successful!",
+      data: others,
+    });
+  }
+}
 
 const loginUser = async (req, res) => {
-    const { identity, password } = req.body
-    const user = await validateUser(identity, password)
-    if (!user) {
-        return res.status(401).send("Invalid crendentials!")
-    }
-    var token = authenticate.getToken({ _id: user._id })
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.json({ success: true, token, status: 'You are successfully logged in' })
-}
+  const { identity, password } = req.body;
+  const user = await validateUser(identity, password);
+  if (!user) {
+    return res.status(401).send("Invalid crendentials!");
+  }
+  var token = authenticate.getToken({ _id: user._id });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({ success: true, token, status: "You are successfully logged in" });
+};
 
 const logoutUser = async (req, res) => {
-    try {
-        console.log(req.user)
-        res.clearCookie('jwt')
-        console.log('logout successful')
+  try {
+    console.log(req.user);
+    res.clearCookie("jwt");
+    console.log("logout successful");
 
-        await req.user.save()
-        res.json({ status: true, message: 'logout successful' })
-    } catch (err) {
-        res.status(500).send(err)
-    }
-}
+    await req.user.save();
+    res.json({ status: true, message: "logout successful" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
 
 async function getAllUser(req, res) {
-    const user = await userModel.find({});
-    res.status(200).json({
-        msg: "all users",
-        data: user
-    })
+  const user = await userModel.find({});
+  res.status(200).json({
+    msg: "all users",
+    data: user,
+  });
 }
 
 async function getOneUser(req, res) {
-    const userId = req.params.id
-    const user = await userModel.findById(userId)
-    if (!user) {
-        return res.status(404).send("User with this id does not exist!")
-    }
-    res.status(200).send(user)
+  const userId = req.params.id;
+  const user = await userModel.findById(userId);
+  if (!user) {
+    return res.status(404).send("User with this id does not exist!");
+  }
+  res.status(200).send(user);
 }
 
 async function updateUserById(req, res) {
-    const id = req.params.id;
-    const bodyToUpdate = req.body;
+  const id = req.params.id;
+  const bodyToUpdate = req.body;
 
-    let user = await userModel.findByIdAndUpdate(id, bodyToUpdate, { new: true });
+  let user = await userModel.findByIdAndUpdate(id, bodyToUpdate, { new: true });
 
-    if (!user) {
-        return res.status(404).send("User does not exit")
-    }
-    res.status(201).json({
-        msg: "User updated Successfully",
-        data: user
-    })
-};
+  if (!user) {
+    return res.status(404).send("User does not exit");
+  }
+  res.status(201).json({
+    msg: "User updated Successfully",
+    data: user,
+  });
+}
 
 async function deleteUserById(req, res) {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    const user = await userModel.findByIdAndDelete(id)
-    if (!user) {
-        return res.status(404).send("Can't delete! user does not exist!")
-    }
-    user.delete()
-    res.json({
-        status: 200,
-        msg: "User deleted successfully!"
-    })
+  const user = await userModel.findByIdAndDelete(id);
+  if (!user) {
+    return res.status(404).send("Can't delete! user does not exist!");
+  }
+  user.delete();
+  res.json({
+    status: 200,
+    msg: "User deleted successfully!",
+  });
 }
 
-
-async function getSellers(req, res, next){
-    const sellers = await userModel.aggregate([
-        {
-            $match: {
-                usertype: "business"
-            }
-        }
-    ])
-    res.status(200).json({status:true, sellers:sellers})
+async function getSellers(req, res, next) {
+  const sellers = await userModel.aggregate([
+    {
+      $match: {
+        usertype: "business",
+      },
+    },
+  ]);
+  res.status(200).json({ status: true, sellers: sellers });
 }
 
-async function getSellerById(req, res){
-    const sellerId = req.params.id
-    const seller = await userModel.findOne({_id: sellerId, usertype: "business"})
+async function getSellerById(req, res) {
+  const sellerId = req.params.id;
+  const seller = await userModel.findOne({
+    _id: sellerId,
+    usertype: "business",
+  });
 
-    res.status(200).json({
-        status: true,
-        seller: seller
-    })
+  res.status(200).json({
+    status: true,
+    seller: seller,
+  });
 }
-
 
 module.exports = {
-    createUser,
-    loginUser,
-    logoutUser,
-    updateUserById,
-    deleteUserById,
-    getAllUser,
-    getOneUser,
-    getSellers,
-    getSellerById
-}
+  createUser,
+  loginUser,
+  logoutUser,
+  updateUserById,
+  deleteUserById,
+  getAllUser,
+  getOneUser,
+  getSellers,
+  getSellerById,
+};
