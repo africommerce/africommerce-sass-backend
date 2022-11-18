535acc76a4c201c0b57e01cb9cd6b4c3c0e22695
Tobi Balogun <tobi.balo@gmail.com>
2022-11-18 12:47:42 +0100

    style: correct style violations - batch 1

--- src/app.js
+++ src/app.js
@@ -1,49 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var passport = require('passport')
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const passport = require('passport')
 
-var indexRouter = require('../routes/index');
-var bodyParser = require('body-parser')
-var { connectToDatabase } = require("./db")
-
-
+const indexRouter = require('../routes/index')
+const bodyParser = require('body-parser')
+const { connectToDatabase } = require('./db')
 
 connectToDatabase()
-var app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, '../views'))
+app.set('view engine', 'jade')
 
-app.use(logger('dev'));
+app.use(logger('dev'))
 // app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, '../public')))
 
-app.use(passport.initialize());
+app.use(passport.initialize())
 
-app.use('/', indexRouter);
-
+app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error', { title: 'Error Details'});
-});
+  res.status(err.status || 500)
+  res.render('error', { title: 'Error Details' })
+})
 
-module.exports = app;
+module.exports = app
