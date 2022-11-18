77fe70c50c9cace102f6543c0c5cd3ec24745a41
Tobi Balogun <tobi.balo@gmail.com>
2022-11-18 14:11:11 +0100

    style: correct style violations - batch 8

--- src/wishListRoute/wishList.js
+++ src/wishListRoute/wishList.js
@@ -1,27 +1,27 @@
-const express = require("express");
+const express = require('express')
 
-const WishListRoute = express.Router();
-const authenticate = require('../../middleware/authenticate');
-const wishList = require("../../model/wishList");
-
+const WishListRoute = express.Router()
+const authenticate = require('../../middleware/authenticate')
+// const wishList = require('../../model/wishList')
 
 // productRoute.use(passport.authenticate('jwt', { session: false }))
 
+const {
+  CreateWishList,
+  getWishLists,
+  deleteWishList,
+} = require('./wishListController')
 
-const {CreateWishList, getWishLists, deleteWishList} = require("./wishListController")
+WishListRoute.route('/')
+  .all((req, res, next) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    next()
+  })
 
+  .post(authenticate.verifyUser, CreateWishList)
+  .get(getWishLists)
 
-WishListRoute.route("/")
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json")
-        next()
-    })
+WishListRoute.route('/:id').delete(deleteWishList)
 
-    .post(authenticate.verifyUser, CreateWishList)
-    .get(getWishLists)
-
-WishListRoute.route("/:id")
-    .delete(deleteWishList)
-
-module.exports = WishListRoute;
+module.exports = WishListRoute
