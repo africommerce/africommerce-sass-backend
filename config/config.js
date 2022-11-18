6e83166f192f108edb13826c69f18edce17b9705
Tobi Balogun <tobi.balo@gmail.com>
2022-11-18 12:51:01 +0100

    style: correct style violations - batch 2

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = {
-    'mongoUrl': process.env.DB_URL,
-    'port': process.env.PORT,
-    'jwtSecret': process.env.JWTSECRET
+  mongoUrl: process.env.DB_URL,
+  port: process.env.PORT,
+  jwtSecret: process.env.JWTSECRET,
 }
