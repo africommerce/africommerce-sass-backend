d3cbd815ecc6393dda23221b17ae9c07476cc453
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-11-21 22:38:14 +0100

    updated controller

--- src/products/utils/helper.js
+++ src/products/utils/helper.js
@@ -7,6 +7,12 @@ exports.pages = (curPage) => {
 
 exports.buildQuery = (queryObj, categoryObj) => {
   let query = {}
+
+  /*Handling sort object*/
+  /*sample of object type required ' -fieldname || fieldname' to filter in ascending or descending order*/
+  const sortBy = queryObj.sort? queryObj.sort.split(',').join(' '): '-createdAt'
+  delete queryObj.sort
+
   if (queryObj.category_name) {
     const category = categoryObj
     if (category) {
@@ -14,5 +20,5 @@ exports.buildQuery = (queryObj, categoryObj) => {
     }
   }
   query = { ...query, ...queryObj }
-  return query
+  return { query, sortBy }
 }
