ef6a4b41548d2c2a1ede76381415034085a2cd6b
Skywonda <princesam3600@gmail.com>
2022-12-03 23:04:52 +0100

    created create order endpoint

---
+++ src/order/orderController.js
@@ -0,0 +1,30 @@
+const Order = require('../../model/order')
+const Product = require('../../model/products')
+
+async function createOrder(req, res) {
+  const user = req.user.id
+  const product = req.body.product
+  let product_owner = await Product.findById(product)
+  if (!product) {
+    return res.status(404).json({
+      msg: 'product to order not found!',
+    })
+  }
+  product_owner = product_owner.populate('owner_id', 'id')
+  if (!product_owner) {
+    return res.status(404).json({
+      msg: 'Product not found!',
+    })
+  }
+
+  const newOrder = new Order({
+    user,
+    product,
+    product_owner
+  })
+  console.log(newOrder)
+}
+
+module.exports = {
+  createOrder,
+}
