5b6177330a46e61e6bca5701fc18ad2bc4637e14
Skywonda <princesam3600@gmail.com>
2023-01-14 00:26:35 +0100

    restructed business user model

---
+++ src/business/buisnessRouter.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const authenticate = require('../../middleware/authenticate')
+
+const {
+  registerBusinessUser,
+  getBusinessUserInfo,
+  updateBusinessInfo,
+  getAllBusinessUser,
+} = require('./businessController')
+
+router.route('/signup').post(authenticate.verifyUser, registerBusinessUser)
+
+router.route('/').get(getAllBusinessUser)
+
+router.route('/:id').get(getBusinessUserInfo)
+
+router
+  .route('/:id')
+  .put(authenticate.verifyUser, authenticate.verifyAuthor, updateBusinessInfo)
+
+module.exports = router
